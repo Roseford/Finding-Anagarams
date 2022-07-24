2752d76782cdf71422f800289b8193e9238fb0a3
Obianuju <obianujuofodu@gmail.com>
2022-07-25 00:19:02 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    
+#    return True
+    if (Word == Anagram):
+        return True      
+    else:
+        return False
+
+Word = sorted(input())
+Anagram = sorted(input())
+
+
+print(find_anagram(Word, Anagram))     
+
+
